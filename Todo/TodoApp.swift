76bdb84dcc49259d3df8bd476f203519fc9a4fbf
Huy Hoang Nguyen <ngxhuyhoang@gmail.com>
2022-02-05 01:10:36 +0700

    feat: init project

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Huy Hoang Nguyen on 05/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
